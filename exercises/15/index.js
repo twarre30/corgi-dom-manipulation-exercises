c62e7104431d76cab992fbdaf624d92745d2e98b
Tiffany <twarre30@gmail.com>
2022-03-30 11:29:38 -0500

    add 14

---
+++ exercises/15/index.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body')
+const img = document.createElement('img')
+img.setAttribute = "https://mymodernmet.com/wp/wp-content/uploads/2020/10/cooper-baby-corgi-dogs-8.jpg"
+body.append(img)
+
+
+//NOT CORRECT
